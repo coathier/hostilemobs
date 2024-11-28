93699a46af123adc01256dbc33225a6d8d113617
coathier <pal.david.miskolczi@gmail.com>
2024-11-28 20:33:50 +0100

    Changed default settings when mobs should be hostile

--- src/main/java/coathier/hostilemobs/HostileMobsConfig.java
+++ src/main/java/coathier/hostilemobs/HostileMobsConfig.java
@@ -5,7 +5,7 @@ import me.shedaniel.autoconfig.annotation.Config;
 
 @Config(name="scalingmobs")
 public class HostileMobsConfig implements ConfigData {
-  public int activeNthDay = 1;
+  public int activeNthDay = 7;
 
   // Basically every try is 10 seconds appart, every try it tries to find a new path to the block, if it's within the same area in N tries it will blow up.
   public int triesBeforeBlowingUp = 5;
